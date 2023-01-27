ac813a88b38feee39718d333b3e34ea0a1ababf5
GideonMissoy <missoykipsang@gmail.com>
2023-01-27 15:55:37 -0800

    I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life.

---
+++ get_flags.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * get_flags - Calculates active flags
+ * @format: Formatted string in which to print the arguments
+ * @i: take a parameter.
+ * Return: Flags
+ */
+
+int get_flags(const char *format, int *i)
+{
+	int q, curr_i;
+	int flags = 0;
+	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
+	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+
+	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	{
+		for (q = 0; FLAGS_CH[q] != '\0'; q++)
+		{
+			if (format[curr_i] == FLAGS_CH[q])
+			{
+				flags |= FLAGS_ARR[q];
+				break;
+			}
+		}
+		if (FLAGS_CH[q] == 0)
+			break;
+	}
+	*i = curr_i - 1;
+
+	return (flags);
+}
