a6487ccd332aaa5748aba727fff4b48a02e4f804
GideonMissoy <missoykipsang@gmail.com>
2023-01-28 18:29:24 -0800

    Updating the main.h file.

--- main.h
+++ main.h
@@ -38,7 +38,7 @@ typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
-va_list list, char buffer[], int flags, int width, int precision, int size;
+		va_list list, char buffer[], int flags, int width, int precision, int size);
 
 /* Functions to print chars and strings */
 int print_char(va_list types, char buffer[],
