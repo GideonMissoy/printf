f80e23184db9ae09094fa9aa794a11c50296a8a4
GideonMissoy <missoykipsang@gmail.com>
2023-01-30 12:14:54 -0800

    Adding print_hexadecimal property.

--- all_printf.c
+++ all_printf.c
@@ -19,9 +19,12 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	fmt_t fmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
-		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
-		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
-		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
+		{'u', print_unsigned}, {'o', print_octal},
+		{'x', print_hexadecimal},
+		{'X', print_hexa_upper}, {'p', print_pointer},
+		{'S', print_non_printable},
+		{'r', print_reverse}, {'R', print_rot13string},
+		{'\0', NULL}
 	};
 
 	for (p = 0; fmt_types[p].fmt != '\0'; p++)
--- functions1.c
+++ functions1.c
@@ -83,6 +83,24 @@ int print_octal(va_list types, char buffer[],
  * @size: Size specifier
  * Return: Number of chars printed
  */
+
+int print_hexadecimal(va_list types, char buffer[],
+		int flags, int width, int precision, int size)
+{
+	return (print_hexa(types, "0123456789abcdef", buffer,
+				flags, 'x', width, precision, size));
+}
+
+/**
+ * print_hexa_upper - Prints an unsigned number in upper hexadecimal notation
+ * @types: List of arguments
+ * @buffer: Buffer array to handle print
+ * @flags: Calculates active flags
+ * @width: get width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
 int print_hexa_upper(va_list types, char buffer[],
 		int flags, int width, int precision, int size)
 {
